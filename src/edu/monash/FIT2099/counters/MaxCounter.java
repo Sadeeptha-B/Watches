09f7f9d33b28829a94ea3a5d2de5f524281fc581
Sadeeptha Bandara <hban0006@student.monash.edu>
2020-04-25 17:07:22 +0800

    Defining field format of the counter, upon instantiation, in the constructor

--- src/edu/monash/FIT2099/counters/MaxCounter.java
+++ src/edu/monash/FIT2099/counters/MaxCounter.java
@@ -2,9 +2,13 @@ package edu.monash.FIT2099.counters;
 
 public class MaxCounter extends Counter {
 	private int max;
+	private final double fieldWidth;
+	private final String fieldFormat;
 	
 	public MaxCounter(int maxValue) {
 		max = maxValue;
+		fieldWidth = Math.ceil(Math.log10(max));
+		fieldFormat = "%0" + String.format("%.0f", fieldWidth) + "d";
 	}
 
 	public void increment() {
@@ -15,14 +19,10 @@ public class MaxCounter extends Counter {
 		}
 	}
 	
-	public int getMax() {
-		return max;
-	}
+
 	
 	@Override
 	public String toString() {
-		double fieldWidth = Math.ceil(Math.log10(this.getMax()));
-		String fieldFormat = "%0" + String.format("%.0f", fieldWidth) + "d";
 		return String.format(fieldFormat, this.getValue());
 	}
 	
